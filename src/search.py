d069e2b11f2d90d79aaff5ed54b9172d222c88e2
Joel Hill <joel.hill@usask.ca>
2015-02-09 13:29:30 -0600

    Main.py removed (unused and misleading). Test lists added to Search

--- src/search.py
+++ src/search.py
@@ -15,6 +15,7 @@ class Search(object):
     vehicle_priority_q = None
     active_vehicles = []
     current_state = None
+    original_package_list = []
 
     """docstring for Search
     N: Number of vehicles.
@@ -28,10 +29,16 @@ class Search(object):
         # initialize state space
         #print "List is:  " + list
         self.StateSpace = S.StateSpace(K, N, H, W, list)
+        self.original_package_list = self.package_list()
         self.vehicle_priority_q = Queue.PriorityQueue(maxsize=0)
         self.populate_v_q()
         self.current_state = state.State(self.StateSpace.vehicles, self.StateSpace.packages, self.StateSpace)
-        self.set_initial_fleet()
+
+        #get inactive vehicle and give package
+        v = self.current_state.inactive_vehicles.get()
+        self.current_state.active_vehicles.append(v)
+        self.current_state.deliver_packages()
+        #self.set_initial_fleet()
 
     def new_vehicle_priority(self, package):
         """
@@ -44,7 +51,7 @@ class Search(object):
             return -1
         close_drop_dist = self.StateSpace.closest_dropoff(package)[0]
         garage_dist = len(self.StateSpace.shortest_path(self.StateSpace.garage, package.pickup))
-        return garage_dist - close_drop_dist
+        return  close_drop_dist - garage_dist
 
     def populate_v_q(self):
         for package in self.StateSpace.packages:
@@ -93,8 +100,8 @@ class Search(object):
     def print_current_state(self):
         self.StateSpace.print_status()
 
-    def print_package_list(self):
-        print self.StateSpace.package_list()
+    def package_list(self):
+        return self.StateSpace.package_list()
 
     def solution(self):
         if self.current_state.finished():
@@ -102,13 +109,25 @@ class Search(object):
         else:
             print "No solution yet. use solve()"
 
-#list1 = [[12, 12], [95, 5], [69, 39], [22, 84], [21, 41]]
-#list2 = [[12, 12], [95, 5], [69, 39], [22, 84], [21, 41]]
+test1 = [[7, 78], [74, 71], [52, 53], [85, 47], [9, 74], [68, 28], [35, 35], [23, 81], [78, 92], [18, 70]]
 
-test1_list = 
+test2 = [[72, 4], [2, 43], [11, 80], [23, 11], [17, 48]]
 
-test = Search(5, 3, 10, 10)
-test1_1vehicle = Search(5, 3, 10, 10)
+#20 packages
+largetest = [[7091, 7142], [1749, 70], [7377, 8915], [9728, 4863], [2730, 9624], [6794, 193], [825, 9058],
+             [850, 5905], [4453, 9832], [1632, 2620], [2476, 987], [2824, 4709], [2614, 8170], [4676, 8712],
+             [9715, 258], [2907, 9347], [7883, 1033], [3554, 1468], [5876, 7044], [5916, 2127]]
+
+#max 3 vehicles used
+largetestlist2 = \
+    [[7337, 9779], [3523, 481], [7051, 2265], [2291, 2881], [1624, 8721], [1581, 3699], [3950, 1277],
+     [3620, 97], [3002, 8282], [6036, 1796], [2142, 6060], [3935, 500], [5463, 7449], [8510, 8159],
+     [7988, 2904], [2108, 7920], [5456, 9253], [4186, 765], [370, 9154], [3972, 471]]
+
+
+
+test = Search(5, 1, 10, 10, test2)
+#test = Search(5, 1, 10, 10, list([[60, 90], [41, 77], [40, 41], [90, 87], [37, 78]]))
 #test2 = Search(5, 1, 10, 10, list(list1))
 #print "Test Space Status"
 #print test.StateSpace.print_status()
@@ -116,7 +135,7 @@ test1_1vehicle = Search(5, 3, 10, 10)
 #print test.current_state
 #print "Test package:  " + str(test.current_state.packages[0])
 print "Package List:  "
-test.print_package_list()
+print test.original_package_list
 dist1 = test.solve()
 #dist2 = test2.solve()
 
